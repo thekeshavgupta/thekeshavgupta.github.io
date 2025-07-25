4eace6deba6ba699a4f4b36378d589250b4175f9
thekeshavgupta <keshavgupta0997@gmail.com>
2025-07-26 01:55:15 +0530

    refactored the code

--- src/components/projects.js
+++ src/components/projects.js
@@ -3,7 +3,7 @@ function Projects() {
     return (
         <div className="projects-main-class">
             <div className='projects-box'>
-                <p className='projects-head'><span class="magic-word"> Projects</span></p>
+                <p className='projects-head'>Side <span class="magic-word"> Projects</span></p>
                 <p className='projects-head-location'>project-heading</p>
                 <p className='overview'>
                     "Passionate about the intersection of data and innovation, I specialize in machine learning, deep learning, and natural language processing (NLP) to craft intelligent systems that solve real-world challenges."
